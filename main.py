5a524c66f4ddabc0974e2c91e66636d0dba81ee3
Ferclemens <69605971+Ferclemens@users.noreply.github.com>
2023-05-29 16:41:32 -0300

    :pushpin: add querys to create tables

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import config
 
 # Establecer la conexión con la base de datos ( connection strig)
 connection = psycopg2.connect(
-    host="localhost",
-    user="postgres",
+    host=config.host,
+    user=config.user,
     password=config.password,
-    database="postgres",
-    port="5432"
+    database=config.database,
+    port=config.port
 )
 
 # Habilitar la confirmación automática de transacciones
@@ -18,13 +18,20 @@ connection.autocommit = True
 # Definimos la funcion para crear tabla en postgres:
 def crear_tabla():
     cursor = connection.cursor()
-    query = "CREATE TABLE customers(customerid INT PRIMARY KEY, name VARCHAR(50), occupation VARCHAR(50), email VARCHAR(50), company VARCHAR(50), phonenumber VARCHAR(20), age INT)"
-    query2 = "CREATE TABLE agents(agentid INT primary key, name VARCHAR(50));"
-    query3 = "CREATE TABLE calls(callid INT primary key, agentid INT, customerid INT, pickedup SMALLINT, duration INT, productsold SMALLINT);"
+#    query = "CREATE TABLE customers(customerid INT PRIMARY KEY, name VARCHAR(50), occupation VARCHAR(50), email VARCHAR(50), company VARCHAR(50), phonenumber VARCHAR(20), age INT)"
+#    query2 = "CREATE TABLE agents(agentid INT primary key, name VARCHAR(50));"
+#    query3 = "CREATE TABLE calls(callid INT primary key, agentid INT, customerid INT, pickedup SMALLINT, duration INT, productsold SMALLINT);"
+    query = "CREATE TABLE IF NOT EXISTS categorias (categoria_id SERIAL PRIMARY KEY, nombre_categoria VARCHAR(50));"
+    query2 = "CREATE TABLE IF NOT EXISTS productos (producto_id SERIAL PRIMARY KEY, nombre_producto VARCHAR(50), categoria_id INTEGER, FOREIGN KEY (categoria_id) REFERENCES categorias(categoria_id), precio DECIMAL);"
+    query3 = "CREATE TABLE IF NOT EXISTS ventas (ventas_id SERIAL PRIMARY KEY, fecha_venta DATE, producto_id INTEGER, FOREIGN KEY (producto_id) REFERENCES productos(producto_id), cantidad_vendida INTEGER, monto_total DECIMAL);"
+    query4 = "CREATE TABLE IF NOT EXISTS clientes (cliente_id SERIAL PRIMARY KEY, nombre_cliente VARCHAR(100), direccion VARCHAR(150), ciudad VARCHAR(50));"
+    query5 = "CREATE TABLE IF NOT EXISTS tienda (tienda_id SERIAL PRIMARY KEY, nombre_tienda VARCHAR(100),ubicacion VARCHAR(150));"
     try:
         cursor.execute(query)
         cursor.execute(query2)
         cursor.execute(query3)
+        cursor.execute(query4)
+        cursor.execute(query5)
         print("La tabla ha sido creada exitosamente")
     except psycopg2.errors.DuplicateTable:
         print("La tabla ya existe")
@@ -81,7 +88,7 @@ def actualizar_datos():
 cur = connection.cursor()
 
 # Execute the SQL command to create a new database
-#cur.execute("CREATE DATABASE dbprueba;")
+#cur.execute("CREATE DATABASE pythonventasdb;")
 
 # Close the cursor and connection
 cur.close()
